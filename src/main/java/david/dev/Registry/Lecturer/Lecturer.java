d7724209725928437360fdcbb733b3b64dad970d
DavidHODs <davidoluwatobi41@gmail.com>
2023-11-22 11:27:06 +0100

    created getters and setters for Lecturer class

--- src/main/java/david/dev/Registry/Lecturer/Lecturer.java
+++ src/main/java/david/dev/Registry/Lecturer/Lecturer.java
@@ -11,4 +11,70 @@ public class Lecturer {
     private String college;
     private String designation;
 
+    public Lecturer(Long id, String name, String email, LocalDate dob,
+                    int age, String college, String designation) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+        this.college = college;
+        this.designation = designation;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
