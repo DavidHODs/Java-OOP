43b356f9d22732a242508e1df6e29f76b62c30a4
DavidHODs <davidoluwatobi41@gmail.com>
2023-11-22 16:54:39 +0100

    implemented function to calculate age

--- src/main/java/david/dev/Registry/Lecturer/Lecturer.java
+++ src/main/java/david/dev/Registry/Lecturer/Lecturer.java
@@ -1,25 +1,33 @@
 package david.dev.Registry.Lecturer;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Lecturer {
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
-    private int age;
     private String college;
     private String designation;
 
+    private String post;
+
     public Lecturer(Long id, String name, String email, LocalDate dob,
-                    int age, String college, String designation) {
+                    String college, String designation, String post) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
         this.college = college;
         this.designation = designation;
+        this.post = post;
+    }
+
+//    calcAge calculates age based on supplied dob
+    public int calcAge() {
+        Period period = Period.between(this.dob, LocalDate.now());
+        return period.getYears();
     }
 
     public Long getId() {
@@ -54,14 +62,6 @@ public class Lecturer {
         this.dob = dob;
     }
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public String getCollege() {
         return college;
     }
@@ -76,5 +76,13 @@ public class Lecturer {
 
     public void setDesignation(String designation) {
         this.designation = designation;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
     }
 }
--- src/main/java/david/dev/Registry/RegistryApplication.java
+++ src/main/java/david/dev/Registry/RegistryApplication.java
@@ -1,13 +1,21 @@
 package david.dev.Registry;
 
+import david.dev.Registry.Lecturer.Lecturer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.time.LocalDate;
+
+//@SpringBootApplication
 public class RegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RegistryApplication.class, args);
+		Lecturer lecturer1 = new Lecturer(12L, "Francis Egno", "fe@email.com", LocalDate.of(1953, 3, 22), "Sciences", "Lecturer II", "Level Adviser");
+
+		int age = lecturer1.calcAge();
+
+		System.out.printf("the age of %s is %d", lecturer1.getName(), age);
+//		SpringApplication.run(RegistryApplication.class, args);
 	}
 
 }
