a71c2dba0ab2f26c10b48c867c5edf82cdf41946
DavidHODs <davidoluwatobi41@gmail.com>
2023-11-22 11:20:53 +0100

    created Lecturer class with associated fields/variables

--- src/main/java/david/dev/Registry/Lecturer/Lecturer.java
+++ src/main/java/david/dev/Registry/Lecturer/Lecturer.java
@@ -1,4 +1,14 @@
 package david.dev.Registry.Lecturer;
 
+import java.time.LocalDate;
+
 public class Lecturer {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private int age;
+    private String college;
+    private String designation;
+
 }
